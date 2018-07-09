30cf2722c79babd67b6a9a9baf868e51619c9ba1
toyama <toyama.jouji@gmail.com>
2018-07-09 15:49:56 +0900

    WIP

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import numpy as np
-from net import Encoder, Color
+from net import Encoder, ColorNet, SetmentationNet
 
 if __name__ == "__main__":
-    encoder = 
+    
+    encoder = Encoder()
+    color_decoder = ColorNet(args.feature_dim)
+    segmentator = SegmentationNet(args.feature_dim)
     criterion = nn.MSELoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate,
-                             weight_decay=1e-5)
+    encoder_optim = torch.optim.Adam(encoder.parameters(), lr=args.encoder_lr, weight_decay=1e-5)
+    color_decoder_optim = torch.optim.Adam(color_decoder.parameters(), lr=args.decoder_lr, weight_decay=1e-5)
+    segment_optim = torch.optim.Adam(segmentator.parameters(), lr=args.segmentator_lr, weight_decay=1e-5)
+
+    data_loader = []
+
+    for n, batch in enumerate(data_loader):
+        feature = encoder(batch)
+        
